9533e5745130352ba6a2ebcdc7427e694b4abfcd
Ajay Yadav <41838237+Ajayay@users.noreply.github.com>
2022-03-21 21:21:18 -0400

    Time: 52 ms (46.11%), Space: 14.1 MB (98.93%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -1,9 +1,4 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import deque
 class Solution:
     def levelOrder(self, root):
         """
@@ -14,19 +9,27 @@ class Solution:
         if not root:
             return levels
         
-        def helper(node, level):
+        level = 0
+        queue = deque([root,])
+        while queue:
             # start the current level
-            if len(levels) == level:
-                levels.append([])
-
-            # append the current node value
-            levels[level].append(node.val)
-
-            # process child nodes for the next level
-            if node.left:
-                helper(node.left, level + 1)
-            if node.right:
-                helper(node.right, level + 1)
+            levels.append([])
+            # number of elements in the current level 
+            level_length = len(queue)
             
-        helper(root, 0)
+            for i in range(level_length):
+                node = queue.popleft()
+                # fulfill the current level
+                levels[level].append(node.val)
+                
+                # add child nodes of the current level
+                # in the queue for the next level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            # go to next level
+            level += 1
+        
         return levels
