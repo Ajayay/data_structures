f3fbaaef2d749266ac8ae761833ead20d2fe38c6
Ajay Yadav <41838237+Ajayay@users.noreply.github.com>
2022-06-19 13:12:46 -0400

    Time: 81 ms (24.75%), Space: 15.3 MB (27.00%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -1,11 +1,41 @@
-from collections import defaultdict
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def widthOfBinaryTree(self, root: TreeNode) -> int:
-        di = defaultdict(list)
-        def dfs(node, level, column):
-            if node:
-                di[level].append(column)
-                dfs(node.left, level+1, column*2)
-                dfs(node.right, level+1, column*2+1)
-        dfs(root, 0 , 0)
-        return max([max(di[level]) - min(di[level]) +1 for level in di])
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if root != None:
+            q = []
+            root.idx = 0
+            q.append(root)
+            ans = 0
+            while len(q):
+                n = len(q)
+                minVal = q[0].idx
+                first = 0
+                last = 0
+                for i in range(n):
+                    curr_id = q[0].idx - minVal
+                    node = q.pop(0)
+                    
+                    if i == 0:
+                        first = curr_id
+                        
+                    if i == n-1:
+                        last = curr_id
+                        
+                    
+                    if node.left:
+                        node.left.idx = 2*(node.idx) + 1
+                        q.append(node.left)
+                    
+                    if node.right:
+                        node.right.idx = 2 * (node.idx)+2
+                        q.append(node.right)
+                        
+                ans = max(ans, last-first+1)
+            return ans
+                
+                
