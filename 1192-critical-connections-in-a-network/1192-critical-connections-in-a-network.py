4a148656d6468ca730694d20240ff63bd7fac77f
Ajay Yadav <41838237+Ajayay@users.noreply.github.com>
2022-07-10 11:12:43 -0400

    Time: 3933 ms (23.72%), Space: 91.7 MB (17.74%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -1,32 +1,65 @@
-import collections
-class Solution(object):
-    def criticalConnections(self, n, connections):
-        def makeGraph(connections):
-            graph = collections.defaultdict(list)
-            for conn in connections:
-                graph[conn[0]].append(conn[1])
-                graph[conn[1]].append(conn[0])
-            return graph
+class Solution:
+    
+    rank = {}
+    graph = defaultdict(list)
+    conn_dict = {}
+    
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
 
-        graph = makeGraph(connections)
-        connections = set(map(tuple, (map(sorted, connections))))
-        rank = [-2] * n
-
-        def dfs(node, depth):
-            if rank[node] >= 0:
-                # visiting (0<=rank<n), or visited (rank=n)
-                return rank[node]
-            rank[node] = depth
-            min_back_depth = n
-            for neighbor in graph[node]:
-                if rank[neighbor] == depth - 1:
-                    continue  # don't immmediately go back to parent. that's why i didn't choose -1 as the special value, in case depth==0.
-                back_depth = dfs(neighbor, depth + 1)
-                if back_depth <= depth:
-                    connections.discard(tuple(sorted((node, neighbor))))
-                min_back_depth = min(min_back_depth, back_depth)
-            rank[node] = n  # this line is not necessary. see the "brain teaser" section below
-            return min_back_depth
+        self.formGraph(n, connections)
+        self.dfs(0, 0)
+        
+        result = []
+        for u, v in self.conn_dict:
+            result.append([u, v])
+        
+        return result
             
-        dfs(0, 0)  # since this is a connected graph, we don't have to loop over all nodes.
-        return list(connections)
+    def dfs(self, node: int, discovery_rank: int) -> int:
+        
+        # That means this node is already visited. We simply return the rank.
+        if self.rank[node]:
+            return self.rank[node]
+        
+        # Update the rank of this node.
+        self.rank[node] = discovery_rank
+        
+        # This is the max we have seen till now. So we start with this instead of INT_MAX or something.
+        min_rank = discovery_rank + 1
+        for neighbor in self.graph[node]:
+            
+            # Skip the parent.
+            if self.rank[neighbor] and self.rank[neighbor] == discovery_rank - 1:
+                continue
+                
+            # Recurse on the neighbor.    
+            recursive_rank = self.dfs(neighbor, discovery_rank + 1)
+            
+            # Step 1, check if this edge needs to be discarded.
+            if recursive_rank <= discovery_rank:
+                del self.conn_dict[(min(node, neighbor), max(node, neighbor))]
+            
+            # Step 2, update the minRank if needed.
+            min_rank = min(min_rank, recursive_rank)
+        
+        return min_rank
+    
+    def formGraph(self, n: int, connections: List[List[int]]):
+        
+        # Reinitialize for each test case
+        self.rank = {}
+        self.graph = defaultdict(list)
+        self.conn_dict = {}
+        
+        # Default rank for unvisited nodes is "null"
+        for i in range(n):
+            self.rank[i] = None
+        
+        for edge in connections:
+            
+            # Bidirectional edges.
+            u, v = edge[0], edge[1]
+            self.graph[u].append(v)
+            self.graph[v].append(u)
+            
+            self.conn_dict[(min(u, v), max(u, v))] = 1
