a00e36b9913db511827cb9d74282c68a983aaba8
Ajay Yadav <41838237+Ajayay@users.noreply.github.com>
2022-09-01 11:06:05 -0400

    Time: 440 ms (62.56%), Space: 16.4 MB (81.53%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -15,7 +15,7 @@ class Solution:
     def dfs(self, grid, i, j):
         if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
             return
-        grid[i][j] = '#'
+        grid[i][j] = '0'
         self.dfs(grid, i+1, j)
         self.dfs(grid, i-1, j)
         self.dfs(grid, i, j+1)
