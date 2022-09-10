d2dcf6a7eed704aa5f124c1207b0587ff4c4eb29
Ajay Yadav <41838237+Ajayay@users.noreply.github.com>
2022-09-10 00:14:01 -0400

    Time: 115 ms (12.65%), Space: 13.9 MB (47.97%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -0,0 +1,35 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 
+        
+        rows= len(grid)
+        cols = len(grid[0])
+        q = []
+        count_fresh = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append((r,c))
+                elif grid[r][c] == 1:
+                    count_fresh+=1
+                    
+        if count_fresh == 0:
+            return 0
+        count = 0
+        while q:
+            count+=1
+            for sz in range(len(q)):
+                rrow,rcol= q.pop(0)
+                for x,y in [(1,0),(0,1),(-1,0),(0,-1)]:
+                    new_row , new_col = rrow+x , rcol+y
+                    if new_row>=0 and new_row < rows and new_col >=0 and new_col<cols:
+                        if grid[new_row][new_col] == 1:
+                            grid[new_row][new_col] = 2
+                            q.append((new_row,new_col))
+                            count_fresh-=1
+            # count+=1
+        if count_fresh == 0:
+            return count-1
+        else:
+            return -1
