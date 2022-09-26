fcaacd1b4da99db524d14186398c6323b62256f0
Ajay Yadav <41838237+Ajayay@users.noreply.github.com>
2022-09-26 16:44:20 -0400

    Time: 186 ms (10.36%), Space: 17.2 MB (78.77%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -1,11 +1,17 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if not intervals:
+            return True
         intervals.sort(key = lambda x: x[0])
-        n = len(intervals)
-        for i in range(1,n):
-            
-            if intervals[i-1][1]>intervals[i][0]:
+        start = intervals[0][0]
+        end = intervals[0][1]
+        i = 1
+        while i < len(intervals):
+            if end>intervals[i][0]:
                 return False
-            
+            else:
+                end = intervals[i][1]
+            i+=1
         return True
+        
         
